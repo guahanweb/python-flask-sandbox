ce31e3e0c42a90e2ab9ad47b5d7ee17bf3608043
Garth Henson <garth@guahanweb.com>
2023-07-19 15:37:22 -0400

    working connection to dynamodb

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -9,5 +9,5 @@
 
 # start up the actual python app for testing
 cd /app
-python -m flask --debug run --extra-files templates:static
+python -m flask --app app:main --debug run --extra-files templates:static
 echo "done... cleaning up!"
--- src/app.py
+++ src/app.py
@@ -1,12 +1,37 @@
 from flask import Flask
 from flask import render_template
 
+import os
 
-app = Flask(__name__)
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
-@app.get("/")
-def home():
-    return render_template('home.html', title="Home")
+import logging
+config = {
+    "DYNAMODB_ENDPOINT": os.getenv("DYNAMODB_ENDPOINT"),
+    "AWS_REGION": os.getenv("AWS_REGION"),
+}
+print(config)
+logger = logging.getLogger(__name__)
+
+def checkTables():
+    endpoint = config['DYNAMODB_ENDPOINT']
+    client = boto3.resource('dynamodb', endpoint_url=endpoint)
+    response = list(client.tables.all())
+    print(response)
+
+def main():
+    checkTables()
+    app = Flask(__name__)
+
+    @app.get("/")
+    def home():
+        return render_template('home.html', title="Home")
+    
+    return app
 
 if __name__ == "__main__":
+    # checkTables()
+    app = main()
     app.run(host='0.0.0.0')
